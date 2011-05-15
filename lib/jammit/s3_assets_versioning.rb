16a02bfe9b74aa4a7ddee332bec92c437020edf6
Christopher Hein <me@christopherhein.com>
2011-05-15 00:51:26 -0700

    forgot to add one to account for 0

--- lib/jammit/s3_assets_versioning.rb
+++ lib/jammit/s3_assets_versioning.rb
@@ -73,7 +73,7 @@ module Jammit
           "#{protocol}#{asset_hostname_ssl}"
         else
           if asset_hostname.is_a?(Array)
-            i = source.hash % asset_hostname.size
+            i = source.hash % asset_hostname.size + 1
             "#{protocol}#{asset_hostname[i]}"
           else
             "#{protocol}#{asset_hostname}"
